89840f64326ad6b084444d6121c7e3671c06cc6b
Aadarsh125 <sahuaadarsh1258@gmail.com>
2026-04-29 11:55:54 +0530

    updated api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 // export const api = "https://dull-gold-clam-tutu.cyclic.app"
 // export const api = "https://lostandfound-api.onrender.com";
-export const api = "http://localhost:8000";
+// export const api = "http://localhost:8000";
+export const api = "https://back2u-backend.onrender.com";
+
 // export const api = "https://lost-and-found-api-nine.vercel.app";
